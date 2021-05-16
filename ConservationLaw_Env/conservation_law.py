198bceaf1832ac608618159fcc932a5b37fd4729
Haifeng Wang <HF_Wang1031@163.com>
2021-05-16 23:06:48 +0800

    None

--- ConservationLaw_Env/conservation_law.py
+++ ConservationLaw_Env/conservation_law.py
@@ -1,18 +1,19 @@
 import numpy as np
 import scipy.integrate as integrate
-import gym 
+# import gym 
+from conservation_law import CL_Solver
 
-class Conservation_law(gym.Env):
-    def __init__(self,):
+# class Conservation_law(gym.Env):
+#     def __init__(self,):
 
-        obseravtion_space = gym.spaces.Box()
-        action_space = gym.spaces.Discrete(2)
+#         obseravtion_space = gym.spaces.Box()
+#         action_space = gym.spaces.Discrete(2)
 
-    def reset(self,):
-        pass
+#     def reset(self,):
+#         pass
 
-    def step(self,action):
-        pass
+#     def step(self,action):
+#         pass
 
-    def render(self,):
-        pass
+#     def render(self,):
+#         pass
